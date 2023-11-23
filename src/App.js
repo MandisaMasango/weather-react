2ac86d527aead0a35ced64d7d2a7fe6e09f6cf3a
MandisaMasango <mandisamasango13@gmail.com>
2023-11-23 15:58:56 +0900

    used the weather component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
+import axios from 'axios'; 
+import Weather from "./Weather";
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Hello World AGAIN number 2</h1>
+        <h1>Hello World</h1>
+        <Weather city="Incheon" />
       </header>
     </div>
   );
